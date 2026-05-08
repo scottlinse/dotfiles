0cd3db317314a5b4996ff86774dc07dcc804a599
Scott Linse <scottlinse@gmail.com>
2026-05-07 22:44:13 -0700

    Update 2026-05-07

--- .config/fish/functions/stowdots.fish
+++ .config/fish/functions/stowdots.fish
@@ -1,12 +1,27 @@
 function stowdots --description 'Restow dotfiles and sync to git'
-    stow -R -d ~/dotfiles -t ~ . $argv
+    set -l do_push false
+    set -l stow_args
+
+    for arg in $argv
+        if test "$arg" = "--push"
+            set do_push true
+        else
+            set -a stow_args $arg
+        end
+    end
+
+    stow -R -d ~/dotfiles -t ~ . $stow_args
     echo "✅ Dotfiles updated."
-    
-    if test "$argv[1]" = "--push"
+
+    if $do_push
         pushd ~/dotfiles
         git add -A
-        git commit -m "Update "(date +%Y-%m-%d)
-        git push
+        if git diff --cached --quiet
+            echo "ℹ️  No changes to commit."
+        else
+            git commit -m "Update "(date +%Y-%m-%d)
+            git push
+        end
         popd
     end
 end
